ca990ca625285664727a63c0c148f4117f27bb6a
catding <catdingtao@gmail.com>
2017-03-13 13:45:03 +0800

    2017-03-13

--- test_boto3_ec2.py
+++ test_boto3_ec2.py
@@ -1,18 +1,23 @@
 import boto3
-import json
+#import json
+
 
 ec2 = boto3.resource('ec2')
 
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+#instances = ec2.instances.filter(
+#    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+instances = ec2.instances.all()
+
 for instance in instances:
     for instance_tags in instance.tags:
         if(instance_tags['Key']=='Name'):
             instance_tags_name=instance_tags['Value']
-    print(instance.id,instance_tags_name,instance.state['Name'],sep='\t', end='\n')
+    print(instance.id,instance_tags_name,instance.state['Name'],instance.vpc_id)
 
 #for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
 #    print(json.dumps(status,indent=1)) 
+
+
 
 
 
